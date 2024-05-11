b0674565c914c2c6fe506204d78b01ad062dde67
Mohammad Jafariyan Khosrowshahi <mohammad.jafariyan7@gmail.com>
2024-05-11 19:32:49 +0430

    m

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'dc9e66c647b9ead8b9f5');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '19d007ce88bbac6a5ec3');
--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-i18n'), 'version' => 'd399b25cffb1a2619159');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-i18n'), 'version' => '712cb3452f076eefcd7e');
